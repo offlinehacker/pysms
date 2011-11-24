4899bc6869adef78ae3dd2693ef084e2f272d48b
offlinehacker <jakahudoklin@gmail.com>
2011-11-24 01:22:49 -0800

    Added init script and cli app

--- najdismsgw/sender.py
+++ najdismsgw/sender.py
@@ -3,33 +3,33 @@ import time
 from sms import NajdiSiSms
 from mail import pygmail
 
-g_mail="somemail@gmail.com"
-g_password="pass"
-s_mail="findme@something.com
-ns_username="username"
-ns_password="pass"
-ns_dest_phone="041992883"
+def main():
+    g_mail="somemail@gmail.com"
+    g_password="pass"
+    s_mail="findme@something.com"
+    ns_username="username"
+    ns_password="pass"
+    ns_dest_phone="041992883"
 
-sms= NajdiSiSms()
-mail= pygmail()
-mail.login(g_mail, g_password)
-
-while(1):
-    mails= []
-    try:
-        mails= mail.get_mails_from(s_mail) 
-    except:
-        mail.login(g_mail, g_password)
-
-    print "Getting mail: %s" % mails
-    for mail_id in mails:
-        print "Mail with id %s" % mail_id
+    sms= NajdiSiSms()
+    mail= pygmail()
+    mail.login(g_mail, g_password)
+    while(1):
+        mails= []
         try:
-            m, data= mail.fetch_mail(mail_id)
+            mails= mail.get_mails_from(s_mail) 
         except:
             mail.login(g_mail, g_password)
-            continue
-        print "Sending sms with data %s" % m["Subject"]
-        sms.send_sms(ns_username, ns_password, ns_dest_phone, m["Subject"])
 
-    time.sleep(10)
+        print "Getting mail: %s" % mails
+        for mail_id in mails:
+            print "Mail with id %s" % mail_id
+            try:
+                m, data= mail.fetch_mail(mail_id)
+            except:
+                mail.login(g_mail, g_password)
+                continue
+            print "Sending sms with data %s" % m["Subject"]
+            sms.send_sms(ns_username, ns_password, ns_dest_phone, m["Subject"])
+
+        time.sleep(10)
--- setup.py
+++ setup.py
@@ -26,4 +26,9 @@ setup(
         "Topic :: Utilities",
         "License :: OSI Approved :: GNU General Public License (GPL)",
     ],
+    entry_points="""
+        [console_scripts]
+        najdismsgw = najdismsgw.sender:main
+        """,
+
 )
