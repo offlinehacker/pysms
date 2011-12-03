8af877bb52e0a8303fc4912e3d3fd51853815b39
Jaka Hudoklin <jakahudoklin@gmail.com>
2011-12-03 16:48:09 +0100

    Added support for splitting smses if they are larger than 160 characters.

--- najdismsgw/sender.py
+++ najdismsgw/sender.py
@@ -3,8 +3,8 @@ import time
 from sms import NajdiSiSms
 from mail import pygmail
 
-def splitCount(s, count):
-    return [''.join(x) for x in zip(*[list(s[z::count]) for z in range(count)])]
+def splitCount(seq, length):
+    return [seq[i:i+length] for i in range(0, len(seq), length)]
 
 def main():
     g_mail="somemail@gmail.com"
@@ -19,28 +19,35 @@ def main():
     mail= pygmail()
     mail.login(g_mail, g_password)
     while(1):
-        mails= []
-        try:
-            mails= mail.get_mails_from(s_mail) 
-        except:
-            mail.login(g_mail, g_password)
-
-        print "Getting mail: %s" % mails
-        for mail_id in mails:
-            print "Mail with id %s" % mail_id
+        for s_mail in s_mails.split(";"):
+            mails= []
             try:
-                m, data= mail.fetch_mail(mail_id)
+                mails= mail.get_mails_from(s_mail) 
             except:
                 mail.login(g_mail, g_password)
-                continue
-            print "Sending sms with data %s" % m["Subject"]
-            if len(m["Subject"])>ns_limit:
-                splits=splitCount(m["Subject"],
-                        ns_limit-(round(len(m["Subject"])/ns_limit/10, 1)+1)*2+2)
-                for x, split in enumerate(splits):
-                    sms.send_sms(ns_username, ns_password, ns_dest_phone, 
-                            str(x)+"/"+str(len(splits))+" "+split)
-            else:
-                sms.send_sms(ns_username, ns_password, ns_dest_phone, m["Subject"])
 
-        time.sleep(10)
+            print "Getting mail: %s" % mails
+            for mail_id in mails:
+                print "Mail with id %s" % mail_id
+                try:
+                    m, data= mail.fetch_mail(mail_id)
+                except:
+                    mail.login(g_mail, g_password)
+                    continue
+                print "Sending sms with data %s" % m["Subject"]
+                if len(m["Subject"])>ns_limit:
+                    print "Slicing needed"
+                    split_count= int(ns_limit-(round(len(m["Subject"])/ns_limit/10, 1)+1)*2-2)
+                    print "Split count", split_count
+                    splits=splitCount(m["Subject"], split_count)
+                    print "Splits:", splits
+                    for x, split in enumerate(splits):
+                        slice_data= str(x)+"/"+str(len(splits))+" "+split
+                        print slice_data
+                        sms.send_sms(ns_username, ns_password, ns_dest_phone, 
+                                slice_data)
+                        time.sleep(2)
+                else:
+                    sms.send_sms(ns_username, ns_password, ns_dest_phone, m["Subject"])
+
+            time.sleep(10)
