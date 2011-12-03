aae97e0c0b887f3dd01661df9d03a2932d85594f
Jaka Hudoklin <jakahudoklin@gmail.com>
2011-12-03 15:54:32 +0100

    Added support for smses larger than 160 chars

--- najdismsgw/sender.py
+++ najdismsgw/sender.py
@@ -3,6 +3,9 @@ import time
 from sms import NajdiSiSms
 from mail import pygmail
 
+def splitCount(s, count):
+    return [''.join(x) for x in zip(*[list(s[z::count]) for z in range(count)])]
+
 def main():
     g_mail="somemail@gmail.com"
     g_password="pass"
@@ -10,6 +13,7 @@ def main():
     ns_username="username"
     ns_password="pass"
     ns_dest_phone="041992883"
+    ns_limit=160
 
     sms= NajdiSiSms()
     mail= pygmail()
@@ -30,6 +34,13 @@ def main():
                 mail.login(g_mail, g_password)
                 continue
             print "Sending sms with data %s" % m["Subject"]
-            sms.send_sms(ns_username, ns_password, ns_dest_phone, m["Subject"])
+            if len(m["Subject"])>ns_limit:
+                splits=splitCount(m["Subject"],
+                        ns_limit-(round(len(m["Subject"])/ns_limit/10, 1)+1)*2+2)
+                for x, split in enumerate(splits):
+                    sms.send_sms(ns_username, ns_password, ns_dest_phone, 
+                            str(x)+"/"+str(len(splits))+" "+split)
+            else:
+                sms.send_sms(ns_username, ns_password, ns_dest_phone, m["Subject"])
 
         time.sleep(10)
