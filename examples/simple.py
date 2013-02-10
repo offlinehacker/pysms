cff6d4a3a1342c48441390522add9ed40ab32b82
Jaka Hudoklin <jakahudoklin@gmail.com>
2013-02-10 02:07:03 +0000

    Added logging

--- examples/simple.py
+++ examples/simple.py
@@ -1,5 +1,17 @@
 # -*- coding: utf-8 -*-
+import logging, sys
 from pysms.providers import NajdiSiSms
 
+loglevel = 'DEBUG'
+
+datefmt = '%b %d %H:%M:%S'
+logformat = '%(asctime)s %(levelname)s pysms: %(message)s'
+
+logging.basicConfig(level=loglevel,
+                    stream=sys.stdout,
+                    format=logformat,
+                    datefmt=datefmt)
+
 provider = NajdiSiSms("username","password")
+print provider.send("51385279", u"čžš")
 print provider.send("51385279", u"čžš")
